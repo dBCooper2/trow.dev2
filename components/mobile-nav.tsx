7b227a3551af12ad542be3cac1030e848691bb52
dBCooper2 <t.rowland711@gmail.com>
2024-12-07 14:40:35 -0600

    working

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import { useState } from "react"
+import { useState } from "react";
 import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "./ui/sheet";
 import { Button } from "./ui/button";
 import { Menu } from "lucide-react";
@@ -9,65 +9,88 @@ import { useRouter } from "next/navigation";
 import { Icons } from "./icons";
 import { siteConfig } from "@/config/site";
 
-
 export function MobileNav() {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    return(
-        <Sheet open={open} onOpenChange={setOpen}>
-            <SheetTrigger asChild>
-                <Button variant="outline" className="w-10 px-0 sm:hidden">
-                    <Menu className="h-5 w-5" />
-                    <span className="sr-only">Toggle Menu</span>
-                </Button>
-            </SheetTrigger>
-            <SheetContent side="right">
-                <SheetTitle>Menu</SheetTitle>
-                <MobileLink onOpenChange={setOpen} href="/" className="flex items-center">
-                <Icons.logo className="mr-2 h-4 w-4" />
-                <span className="font-bold">{siteConfig.name}</span>
-                </MobileLink>
-                <div className="flex flex-col gap-3 mt-3">
-                    <hr />
-                    <MobileLink onOpenChange={setOpen} href="/articles">Articles</MobileLink>
-                    <MobileLink onOpenChange={setOpen} href="/projects">Projects</MobileLink>
-                    <MobileLink onOpenChange={setOpen} href="/about">About</MobileLink>
-                    <hr />
-                    <Link target="_blank" rel="noreferrer" href={siteConfig.links.github}>Github</Link>
-                    <Link target="_blank" rel="noreferrer" href={siteConfig.links.instagram}>Instagram</Link>
-                    <Link target="_blank" rel="noreferrer" href={siteConfig.links.linkedin}>LinkedIn</Link>
-                </div>
-            </SheetContent>
-        </Sheet>
-    );
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
+        <Button variant="outline" className="w-10 px-0 sm:hidden">
+          <Menu className="h-5 w-5" />
+          <span className="sr-only">Toggle Menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="right">
+        <SheetTitle>Menu</SheetTitle>
+        <MobileLink
+          onOpenChange={setOpen}
+          href="/"
+          className="flex items-center"
+        >
+          <Icons.logo className="mr-2 h-4 w-4" />
+          <span className="font-bold">{siteConfig.name}</span>
+        </MobileLink>
+        <div className="flex flex-col gap-3 mt-3">
+          <hr />
+          <MobileLink onOpenChange={setOpen} href="/articles">
+            Articles
+          </MobileLink>
+          <MobileLink onOpenChange={setOpen} href="/projects">
+            Projects
+          </MobileLink>
+          <MobileLink onOpenChange={setOpen} href="/about">
+            About
+          </MobileLink>
+          <hr />
+          <Link target="_blank" rel="noreferrer" href={siteConfig.links.github}>
+            Github
+          </Link>
+          <Link
+            target="_blank"
+            rel="noreferrer"
+            href={siteConfig.links.instagram}
+          >
+            Instagram
+          </Link>
+          <Link
+            target="_blank"
+            rel="noreferrer"
+            href={siteConfig.links.linkedin}
+          >
+            LinkedIn
+          </Link>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
 }
 
 // handle the mobile nav opening and closing
 interface MobileLinkProps extends LinkProps {
-    children: React.ReactNode;
-    onOpenChange?: (open: boolean) => void; // option that takes in a bool to change the status to open, returns void
-    className?: string;
+  children: React.ReactNode;
+  onOpenChange?: (open: boolean) => void; // option that takes in a bool to change the status to open, returns void
+  className?: string;
 }
 
 function MobileLink({
-    href,
-    onOpenChange,
-    children,
-    className,
-    ...props
+  href,
+  onOpenChange,
+  children,
+  className,
+  ...props
 }: MobileLinkProps) {
-    const router= useRouter();
-    return (
-        <Link
-            href={href}
-            onClick={() => {
-                router.push(href.toString())
-                onOpenChange?.(false) // "?." only runs the function if it knows it exists
-            }}
-            className={className}
-            {...props}
-            >
-                {children}
-            </Link>
-    );
+  const router = useRouter();
+  return (
+    <Link
+      href={href}
+      onClick={() => {
+        router.push(href.toString());
+        onOpenChange?.(false); // "?." only runs the function if it knows it exists
+      }}
+      className={className}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
 }
